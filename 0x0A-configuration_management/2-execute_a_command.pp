14f043cd048acfebe62fea0a190dcd1a1462c657
Boul3ez85 <benrebah.moez@gmail.com>
2020-08-03 16:56:06 +0100

    kill process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # using Puppet to create a manifest that kill a precess
 
-exec { 'pkill':
-  command  => 'pkill killmenow',
-  path     => '/usr/bin:/usr/sbin:/bin',
+exec { 'killmenow':
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin',],
 }
