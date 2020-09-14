b36ed802b25d9013ebc4537acafdb3a476bc0eb7
Boul3ez85 <https://github.com/Boul3ez85>
2020-09-14 20:33:44 +0100

    fix pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,9 +6,11 @@ import sys
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos_list = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    todos_list = requests.get(url + "todos",
+                              params={"userId": sys.argv[1]}).json()
 
-    progress = [t.get("title") for t in todos_list if t.get("completed") is True]
+    progress = [t.get("title") for t in todos_list
+                if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(progress), len(todos_list)))
 
