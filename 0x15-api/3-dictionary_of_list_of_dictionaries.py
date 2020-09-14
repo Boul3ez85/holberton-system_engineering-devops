da17617306b4d56f6ea0f14037ff040d227bbf84
Boul3ez85 <https://github.com/Boul3ez85>
2020-09-14 22:37:41 +0100

    fix inner dictionaries kies order

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Exports to-do list information of all employees to JSON format."""
+
 import json
 import requests
 
@@ -7,12 +8,12 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
 
-    with open("todo_all_employees.json", "w") as jsonfile:
+    with open("todo_all_employees.json", "w") as f:
         json.dump({
-            u.get("id"): [{
+            i.get("id"): [{
                 "task": t.get("title"),
                 "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+                "username": i.get("username")
+            } for task in requests.get(url + "todos",
+                                    params={"userId": i.get("id")}).json()]
+            for i in users}, f)
