0cc7f634790eb129ddc043b23114902e64bc9912
Boul3ez85 <https://github.com/Boul3ez85>
2020-09-14 17:30:57 +0100

    fix code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-""" using this REST API, for a given employee ID,
-    returns information about his/her TODO list progress."""
-
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
 if __name__ == "__main__":
-
-    url = 'https://jsonplaceholder.typicode.com/'
+    url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos_list = requests.get(url + "todos",
-                              params={"userId": sys.argv[1]}).json()
+    todos_list = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    progress = [task.get('title') for task in todos_list
-                if task.get('progress') is True]
-    print('Employee {} is done with tasks({}/{}):'.format(user.get('name'),
-                                                          len(progress),
-                                                          len(todos_list)))
-    for task in progress:
-        print('\t {}'.format(task))
+    progress = [t.get("title") for t in todos_list if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(progress), len(todos_list)))
+
+    for a in progress:
+        print("\t {}".format(a))
