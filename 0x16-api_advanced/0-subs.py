8ecce2cf202c4c8310cda53a651ccf8358ee7a9c
Boul3ez85 <https://github.com/Boul3ez85>
2020-09-17 15:59:08 +0100

    number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0)\
+                    Gecko/20100101 Firefox/80.0' +
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36\
+                    (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
+            }
+    req = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit),
+                       headers=headers,
+                       allow_redirects=False)
+
+    if type(subreddit) is not str or subreddit is None:
+        return (0)
+    if req.status_code >= 300:
+        return (0)
+
+    json = req.json()
+    d = json.get('data', {})
+    return (d.get('subscribers', 0))
