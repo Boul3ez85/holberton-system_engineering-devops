43b9f9889599abd2f08d6acbb2d79f6c967527fc
Boul3ez85 <benrebah.moez@gmail.com>
2020-09-30 07:52:01 +0100

    fix puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,6 @@
 # a puppet script to fix a typo error that causes a 500 error.
 
-exec { '/var/www/html/wp-setting.php':  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",}
+exec { '/var/www/html/wp-setting.php':
+  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+}
