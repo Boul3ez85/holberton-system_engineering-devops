55af2a48cdf82284abd60337bb349f86da44e93c
Boul3ez85 <benrebah.moez@gmail.com>
2020-09-30 07:45:59 +0100

    test

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,1 @@
-exec { '/var/www/html/wp-setting.php':
-  path    =>  [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],\
-command   =>\
-  "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",}
+exec { '/var/www/html/wp-setting.php':  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",}
