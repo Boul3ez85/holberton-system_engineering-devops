74814147c4335df82f0d1762533ea1ac7dfb8939
Boul3ez85 <benrebah.moez@gmail.com>
2020-09-30 07:49:46 +0100

    add comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,3 @@
+# a puppet script to fix a typo error that causes a 500 error.
+
 exec { '/var/www/html/wp-setting.php':  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",}
