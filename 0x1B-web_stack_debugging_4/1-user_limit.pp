6e8056326f000603b1a6be7dbb29ae230ad2d616
Moez <benrebah.moez@gmail.com>
2020-10-12 15:34:26 +0100

    increase user limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# allow holberton's user to login and open files.
+
+exec { 'increase-hard-file-limit':
+  command => 'sed -i "/holberton hard/s/5/30000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+exec { 'increase-soft-file-limit':
+  command => 'sed -i "/holberton soft/s/4/30000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
